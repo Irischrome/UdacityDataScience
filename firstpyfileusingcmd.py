ba79d3ceff451a545c403e89c27593b7806d3a47
IrisChrome <shubhi.0719@gmail.com>
2021-06-22 23:43:56 +0530

    first commit

---
+++ firstpyfileusingcmd.py
@@ -0,0 +1,7 @@
+hello world!
+Use exit() or Ctrl-Z plus Return to exit
+Use exit() or Ctrl-Z plus Return to exit
+Use exit() or Ctrl-Z plus Return to exit
+<built-in function dir>
+Use exit() or Ctrl-Z plus Return to exit
+Use exit() or Ctrl-Z plus Return to exit
